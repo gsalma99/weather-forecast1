b2e00263912d6f141e7d9ea24f2339485b7af398
gsalma99 <salmagonzalezzz10011@gmail.com>
2023-02-06 23:45:32 -0600

    finished the search history and working on the weather section

--- Assets/index.js
+++ Assets/index.js
@@ -19,3 +19,59 @@ var appendHistoryToContainer = (searchHistoryEntry) => {
     var searchHistoryContainerEl = $("#previous-history-container");
     searchHistoryContainerEl.append(searchHistoryContainer);
 };
+
+var updateSavedSearches = (cityName) => {
+    var previousSavedSearches = localStorage.getItem("savedSearches");
+    savedSearches = previousSavedSearches
+        ? JSON.parse(previousSavedSearches)
+        : [];
+    savedSearches.push(cityName);
+    localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
+};
+
+var resetSearchInput = () => {
+    $("#search-input").val("");
+  };
+
+var searchHistoryList = (cityName) => {
+    $(".past-search:contains(" + cityName +")").remove();
+    var searchHistoryEntry = createSearchHistoryEntry(cityName);
+    var searchHistoryContainer =
+        createSearchHistoryContainer(searchHistoryEntry);
+    appendSearchHistoryToContainer(searchHistoryContainer);
+    updateSavedSearches(cityName);
+    resetSearchInput();
+};
+
+var getSavedSearchHistory = () => localStorage.getItem("savedSearches");
+
+var parseSavedSearchHistory = (savedSearchHistory) =>
+  savedSearchHistory ? JSON.parse(savedSearchHistory) : false;
+
+var loadSearchHistory = () => {
+    var savedSearchHistory = getSavedSearchHistory();
+    var parsedSavedSearchHistory = parseSavedSearchHistory(savedSearchHistory);
+    if (!parsedSavedSearchHistory) {
+        return false;
+    }
+    parsedSavedSearchHistory.forEach(searchHistoryList);
+};
+
+var todayWeatherSection = async function (cityName) {
+    try {
+        var respone = await fetch(
+            'https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}'
+        );
+        var { coord } = await respone.json();
+        var oneCallReponse = await fetch(
+            'https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${apiKey}'
+        );
+        var weatherData = await oneCallReponse.json();
+
+        searchHistoryList(cityName);
+
+        var currentWeatherContainer = $("#today-weather-conatiner");
+        currentWeatherContainer.addClass("today-weather-container");
+        
+    }
+}
